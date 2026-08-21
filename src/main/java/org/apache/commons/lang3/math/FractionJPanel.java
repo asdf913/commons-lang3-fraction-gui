5b623f9ce3d11b0b6a36c32283b6f18a18417a92
lau-ka-ho <asdf913@gmail.com>
2026-08-21 13:46:26 +0800

    Apply rule "Cognitive Complexity of methods should not be too high" Update "toMathML(java.lang.String,java.lang.String,java.lang.String)" method in "org.apache.commons.lang3.math.FractionJPanel" class

--- src/main/java/org/apache/commons/lang3/math/FractionJPanel.java
+++ src/main/java/org/apache/commons/lang3/math/FractionJPanel.java
@@ -1011,7 +1011,7 @@ public class FractionJPanel extends JPanel
 		if (Boolean.logicalOr(and(numerator, FractionJPanel::isValidString, StringUtils::isNotBlank),
 				and(denominator, FractionJPanel::isValidString, StringUtils::isNotBlank))) {
 			//
-			if (StringUtils.isEmpty(whole) && startsWith(numerator, "-")) {
+			if (Boolean.logicalAnd(StringUtils.isEmpty(whole), startsWith(numerator, "-"))) {
 				//
 				sb.append(String.format("<%1$s>%2$s</%1$s>", "mi", "-"));
 				//
