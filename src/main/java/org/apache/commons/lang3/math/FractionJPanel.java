dbcc738a9a641a8f6f43a2d238165a5f39ada2f8
lau-ka-ho <asdf913@gmail.com>
2026-08-29 13:35:34 +0800

    Apply rule "Cognitive Complexity of methods should not be too high" Update "init()" method in "org.apache.commons.lang3.math.FractionJPanel" class

--- src/main/java/org/apache/commons/lang3/math/FractionJPanel.java
+++ src/main/java/org/apache/commons/lang3/math/FractionJPanel.java
@@ -385,7 +385,7 @@ public class FractionJPanel extends JPanel
 		//
 		final ListCellRenderer lcr2 = (jcbColor = new JComboBox<>(cbmColor = new DefaultComboBoxModel<>(
 				testAndApply(Objects::nonNull, Color.class.getDeclaredFields(), Arrays::stream, null)
-						.filter(f -> isStatic(f) && Objects.equals(getType(f), getDeclaringClass(f))
+						.filter(f -> Boolean.logicalAnd(isStatic(f), Objects.equals(getType(f), getDeclaringClass(f)))
 								&& Objects.equals(StringUtils.upperCase(getName(f)), getName(f)))
 						.sorted((a, b) -> StringUtils.compare(getName(a), getName(b), true))
 						.collect(LinkedHashMap::new, (a, b) -> put(a, b, Narcissus.getStaticField(b)), Map::putAll)
